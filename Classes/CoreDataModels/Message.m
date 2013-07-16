b6382cfdc916bdc53ba69230833e7297fe085af1
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2013-07-17 01:54:29 +0400

    core data model and generated classes added

---
+++ Classes/CoreDataModels/Message.m
@@ -0,0 +1,15 @@
+#import "Message.h"
+
+
+@interface Message ()
+
+// Private interface goes here.
+
+@end
+
+
+@implementation Message
+
+// Custom logic goes here.
+
+@end
---
+++ Classes/CoreDataModels/Private/_Message.h
@@ -0,0 +1,59 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to Message.h instead.
+
+#import <CoreData/CoreData.h>
+
+
+extern const struct MessageAttributes {
+	__unsafe_unretained NSString *messageStr;
+} MessageAttributes;
+
+extern const struct MessageRelationships {
+} MessageRelationships;
+
+extern const struct MessageFetchedProperties {
+} MessageFetchedProperties;
+
+
+
+
+@interface MessageID : NSManagedObjectID {}
+@end
+
+@interface _Message : NSManagedObject {}
++ (id)insertInManagedObjectContext:(NSManagedObjectContext*)moc_;
++ (NSString*)entityName;
++ (NSEntityDescription*)entityInManagedObjectContext:(NSManagedObjectContext*)moc_;
+- (MessageID*)objectID;
+
+
+
+
+
+@property (nonatomic, strong) NSString* messageStr;
+
+
+
+//- (BOOL)validateMessageStr:(id*)value_ error:(NSError**)error_;
+
+
+
+
+
+
+@end
+
+@interface _Message (CoreDataGeneratedAccessors)
+
+@end
+
+@interface _Message (CoreDataGeneratedPrimitiveAccessors)
+
+
+- (NSString*)primitiveMessageStr;
+- (void)setPrimitiveMessageStr:(NSString*)value;
+
+
+
+
+@end
